e4c5235fbcfeb3af40779fafed60185bb909fcea
Łukasz Drzensla <drzensla@student.agh.edu.pl>
2023-01-14 15:41:26 +0100

    first commit

---
+++ Objects/tpm.d
@@ -0,0 +1,11 @@
+.\objects\tpm.o: tpm.c
+.\objects\tpm.o: tpm.h
+.\objects\tpm.o: frdm_bsp.h
+.\objects\tpm.o: E:\Software\keilpacks\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL05Z4.h
+.\objects\tpm.o: E:\Software\keilpacks\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm0plus.h
+.\objects\tpm.o: E:\Software\keil\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\tpm.o: E:\Software\keilpacks\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\tpm.o: E:\Software\keilpacks\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\tpm.o: E:\Software\keilpacks\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\tpm.o: E:\Software\keilpacks\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\system_MKL05Z4.h
+.\objects\tpm.o: led.h
---
+++ Objects/tsi.d
@@ -0,0 +1,10 @@
+.\objects\tsi.o: tsi.c
+.\objects\tsi.o: tsi.h
+.\objects\tsi.o: frdm_bsp.h
+.\objects\tsi.o: E:\Software\keilpacks\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL05Z4.h
+.\objects\tsi.o: E:\Software\keilpacks\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm0plus.h
+.\objects\tsi.o: E:\Software\keil\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\tsi.o: E:\Software\keilpacks\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\tsi.o: E:\Software\keilpacks\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\tsi.o: E:\Software\keilpacks\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\tsi.o: E:\Software\keilpacks\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\system_MKL05Z4.h
