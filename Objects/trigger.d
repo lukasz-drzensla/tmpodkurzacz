a5f7e5d339f13032d39a6d8cee2a6ca12e9cd1d7
lukasz-drzensla <117318345+lukasz-drzensla@users.noreply.github.com>
2023-01-26 22:38:36 +0100

    final changes

--- Objects/trigger.d
+++ Objects/trigger.d
@@ -1,10 +1,10 @@
 .\objects\trigger.o: trigger.c
 .\objects\trigger.o: trigger.h
 .\objects\trigger.o: frdm_bsp.h
-.\objects\trigger.o: E:\Software\keilpacks\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL05Z4.h
-.\objects\trigger.o: E:\Software\keilpacks\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm0plus.h
-.\objects\trigger.o: E:\Software\keil\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\trigger.o: E:\Software\keilpacks\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h
-.\objects\trigger.o: E:\Software\keilpacks\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h
-.\objects\trigger.o: E:\Software\keilpacks\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armcc.h
-.\objects\trigger.o: E:\Software\keilpacks\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\system_MKL05Z4.h
+.\objects\trigger.o: C:\Users\Lenovo\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL05Z4.h
+.\objects\trigger.o: C:\Users\Lenovo\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm0plus.h
+.\objects\trigger.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\trigger.o: C:\Users\Lenovo\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\trigger.o: C:\Users\Lenovo\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\trigger.o: C:\Users\Lenovo\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\trigger.o: C:\Users\Lenovo\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\system_MKL05Z4.h
